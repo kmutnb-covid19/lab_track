aa3238439df99fbd7bd9f475765309ee38e247ef
Nattakrit Jatupattaradit <s6101012610037@email.kmutnb.ac.th>
2020-05-31 03:46:32 +0700

    Change CharField to DateTimeField in History model, Logout user automatically when check out.

--- kmutnbtrackapp/models.py
+++ kmutnbtrackapp/models.py
@@ -4,23 +4,30 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Lab(models.Model):
-    lab_name = models.CharField(max_length=300,null=True)
+    lab_name = models.CharField(max_length=300, null=True)
     amount_people = models.IntegerField(blank=True)
+
     def __str__(self):
         return self.lab_name
+
+
 class StudentID(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     student_id = models.CharField(max_length=13, blank=True)
+
     def __str__(self):
         return self.student_id
+
+
 class History(models.Model):
-#   id_by_date = models.อะไรสักอย่าง
-    Student = models.ForeignKey(StudentID,on_delete=models.CASCADE)
+    #   id_by_date = models.อะไรสักอย่าง
+    Student = models.ForeignKey(StudentID, on_delete=models.CASCADE)
     lab = models.ForeignKey(Lab, on_delete=models.CASCADE)
-    lab_name = models.CharField(max_length=300,null=True)
-    checkin = models.CharField(max_length=13,null=True)
-    checkout = models.CharField(max_length=13,null=True)
-    student_name= models.CharField(max_length=50,blank=True)
+    lab_name = models.CharField(max_length=300, null=True)
+    checkin = models.DateTimeField(null=True)
+    checkout = models.DateTimeField(null=True)
+    student_name = models.CharField(max_length=50, blank=True)
     student_ids = models.CharField(max_length=13, blank=True)
+
     def __str__(self):
-        return self.lab_name
+        return str(self.checkin.date()) + " " + self.lab_name + " " + self.student_name

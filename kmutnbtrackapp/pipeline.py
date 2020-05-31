44adebcdf0d5194837dcbbcd8d4254501e076a80
Nattakrit Jatupattaradit <s6101012610037@email.kmutnb.ac.th>
2020-05-31 21:03:44 +0700

    Force user to checkout before checkin
    Co-Authored-By: watcharawut007 <watcharawut007@users.noreply.github.com>

--- kmutnbtrackapp/pipeline.py
+++ kmutnbtrackapp/pipeline.py
@@ -1,15 +1,17 @@
 # pipeline is custom function use when user logged in with facebook
 
 import re
+
 from django.contrib.auth.models import User
-from kmutnbtrackapp.models import StudentID
+
+from kmutnbtrackapp.models import Person
 
 
 # create user UserInfo object
 def get_student_id(backend, user, response, *args, **kwargs):
     if response.get('email').startswith('s') and str(response.get('email')[1]).isdigit():
         student_id = re.findall(r'\d+', response.get('email'))
-        if not StudentID.objects.filter(student_id=student_id[0]).exists():
+        if not Person.objects.filter(student_id=student_id[0]).exists():
             username = (response.get('email')).split("@")[0]
             user = User.objects.get(username=username)
-            StudentID.objects.create(user=user, student_id=student_id[0])
+            Person.objects.create(user=user, student_id=student_id[0])

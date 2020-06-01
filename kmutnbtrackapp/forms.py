38416b541831fbdd8b8c1e00b4bd1432456d4079
Nattakrit Jatupattaradit <s6101012610037@email.kmutnb.ac.th>
2020-06-01 17:47:53 +0700

    Add signup and login by username & password for stranger

--- kmutnbtrackapp/forms.py
+++ kmutnbtrackapp/forms.py
@@ -1,4 +1,15 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
 from .models import *
 
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    email = forms.EmailField(max_length=150)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2', 'first_name', 'last_name', 'email',)
 

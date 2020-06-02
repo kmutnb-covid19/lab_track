5cd01574023e03305987f5d1bf015413ed5ecf31
Watcharawut007 <s6101012630160@email.kmutnb.ac.th>
2020-06-03 01:57:01 +0700

    delay check in 1 hour (not complete version)
    1.เพิ่ม if ถ้าcheck in lab นี้ครั้งแรกสามารถcheck inได้เลย
    2.เพิ่ม if ว่าถ้าเวลาปัจจุบันมากกว่าเวลาcheckin ล่าสุดให้ check in ได้ หรือวันทีั่ check in ล่าสุดกับวันปัจจุบันเป็นคนละวันก็ให้check in ได้
    เพิ่มฟังก์ชัน checkin ใน person
    
    ปัญหา
    เวลาไม่ตรงกัน โดยเวลาที่ดึงมาจะช้ากว่า 7 ชั่วโมงถ้าเอาไปเทสอาจจะมีปัญหานะ

--- kmutnbtrackapp/models.py
+++ kmutnbtrackapp/models.py
@@ -1,35 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+import datetime
 
 # Create your models here.
 class Lab(models.Model):
-    name = models.CharField(max_length=300, null=True)
+    name = models.CharField(max_length=300,null=True)
     amount = models.IntegerField(blank=True)
-
     def __str__(self):
         return self.name
-
-
 class Person(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=50, null=True)
-    last_name = models.CharField(max_length=50, null=True)
-    email = models.EmailField(blank=True, null=True)
     student_id = models.CharField(max_length=13, blank=True)
-    check_in_status = models.BooleanField(blank=True, default=False)
-    is_student = models.BooleanField(blank=True, default=True)
-
+    check_in_status = models.BooleanField(blank=True,default=False)
+    name= models.CharField(max_length=60, blank=True)
+    tel = models.CharField(max_length=10, blank=True)
     def __str__(self):
-        return self.user.first_name + " " + self.user.last_name + " " + self.student_id
-
-
+        return self.student_id
+    def check_in(self):
+        self.check_in_status = True
+        self.save()
+    def check_out(self):
+        self.check_in_status = False
+        self.save()
 class History(models.Model):
-    #   id_by_date = models.อะไรสักอย่าง
-    person = models.ForeignKey(Person, on_delete=models.CASCADE, null=True)
+#   id_by_date = models.อะไรสักอย่าง
+    person = models.ForeignKey(Person,on_delete=models.CASCADE)
     lab = models.ForeignKey(Lab, on_delete=models.CASCADE)
-    checkin = models.DateTimeField(null=True)
+    checkin = models.DateTimeField(null=True,auto_now_add=True)
     checkout = models.DateTimeField(null=True)
-
     def __str__(self):
-        return str(self.checkin.date()) + " " + self.lab.name + " " + self.person.user.first_name
+        return self.lab.name
+
+

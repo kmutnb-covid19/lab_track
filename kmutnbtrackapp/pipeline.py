617763696141a23db4d271da26dbc184d06ee3e6
Nattakrit Jatupattaradit <s6101012610037@email.kmutnb.ac.th>
2020-06-07 22:48:07 +0700

    Create Person object when login with non KMUTNB account
    เพิ่มเงื่อนไขใน pipeline.py ให้สร้าง object Person เมื่อ login ด้วย ID Google ที่ไม่ใช่ของมหาลัย (@email.kmutnb.ac.th)

--- kmutnbtrackapp/pipeline.py
+++ kmutnbtrackapp/pipeline.py
@@ -9,12 +9,17 @@ from kmutnbtrackapp.models import Person
 
 # create user UserInfo object
 def get_student_id(backend, user, response, *args, **kwargs):
+    username = (response.get('email')).split("@")[0]
+    first_name = (response.get('name')).split(" ")[0]
+    last_name = (response.get('name')).split(" ")[1]
     if response.get('email').startswith('s') and str(response.get('email')[1]).isdigit():
         student_id = re.findall(r'\d+', response.get('email'))
         if not Person.objects.filter(student_id=student_id[0]).exists():
-            username = (response.get('email')).split("@")[0]
-            first_name = (response.get('name')).split(" ")[0]
-            last_name = (response.get('name')).split(" ")[1]
             user = User.objects.get(username=username)
             Person.objects.create(user=user, student_id=student_id[0], first_name=first_name, last_name=last_name,
                                   is_student=True, email=response.get('email'))
+    else:
+        if not Person.objects.filter(student_id=(response.get('email')).split("@")[0]).exists():
+            user = User.objects.get(username=username)
+            Person.objects.create(user=user, first_name=first_name, last_name=last_name, is_student=True,
+                                  email=response.get('email'))

823615de21676c078aa49aedd70e287e5e290bd3
Pichet <bestoinw@hotmail.com>
2020-06-11 02:20:54 +0700

    move images to /static, rename pages to lowercase, edit in pages to use static

--- kmutnbtrack/settings.py
+++ kmutnbtrack/settings.py
@@ -148,6 +148,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static/'),
+)
 
 DEFAULT_FROM_EMAIL = 'KMUTNB labtrack <noreply@cony.codes>'
 EMAIL_USE_TLS = True

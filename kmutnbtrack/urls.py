43990bf6f791d9c0d6234c95db2f1566780b46f4
Pichet <bestoinw@hotmail.com>
2020-06-11 02:20:54 +0700

    move images to /static, rename pages to lowercase, edit in pages to use static

--- kmutnbtrack/urls.py
+++ kmutnbtrack/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
@@ -35,3 +36,4 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(next_page='/logout_success'), name="logout"),
     path("logout_success", TemplateView.as_view(template_name="Page/check_out_success.html"))
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)

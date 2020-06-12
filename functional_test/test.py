9730d8966bcd357aef846c7c57ec44d721f6754b
Nattakrit Jatupattaradit <s6101012610037@email.kmutnb.ac.th>
2020-06-13 00:09:07 +0700

    increase sleep time in test

--- functional_test/test.py
+++ functional_test/test.py
@@ -66,38 +66,38 @@ class LoginTest(LiveServerTestCase):
 
     def test_user_can_check_in(self):
         self.browser.get(self.live_server_url + "/lab/" + str(self.lab_com_hash))
-        time.sleep(1)
+        time.sleep(10)
 
         google_button = self.browser.find_element_by_id("google_sign_in_button")
         google_button.click()
-        time.sleep(5)
+        time.sleep(10)
 
         username = self.browser.find_element_by_id("identifierId")
         username.send_keys("functionaltest172@gmail.com")
-        time.sleep(5)
+        time.sleep(10)
 
         username.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(10)
 
         password = self.browser.find_element_by_name("password")
         password.send_keys('Djangotest123')
-        time.sleep(5)
+        time.sleep(10)
 
         password.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(10)
 
         room = self.browser.find_element_by_id('room_name').text
         self.assertIn('Computer Lab', room)
 
         self.browser.find_element_by_xpath("//select[@name='check_out_select_time']/option[text()='อื่นๆ']").click()
-        time.sleep(5)
+        time.sleep(10)
 
         time_check_out = self.browser.find_element_by_name("check_out_time")
         time_check_out.send_keys("23:59")
-        time.sleep(5)
+        time.sleep(10)
 
         check_in_button = self.browser.find_element_by_id('check_in_button').click()
-        time.sleep(5)
+        time.sleep(10)
 
         check_out_time = self.browser.find_element_by_id('check_out_time_id').text
         self.assertIn('11:59 PM', check_out_time)

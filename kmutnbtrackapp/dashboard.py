a2b662b3c382bcd9babc7f7b1fee8b17960cfaa7
sunjrp <s6101012630054@email.kmutnb.ac.th>
2020-06-16 23:43:20 +0700

    add liner chart in dashboard and add dashboard to history/change_list

--- kmutnbtrackapp/dashboard.py
+++ kmutnbtrackapp/dashboard.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import time
 
 
 def get_data_metadata():
@@ -25,3 +26,29 @@ def date_hook(json_dict):
         except:
             pass
     return json_dict
+
+
+def prepare_pie_data(meta_data):
+    # ['Task', 'Hours per Day'] เอาไว้ทำอะไรก็ไม่รู้แต่ไม่มีทำกราฟไม่ได้ 55555555
+    data = [['Task', 'Hours per Day']]
+    for lab_name in meta_data['lab']:
+        data.append([lab_name, sum(meta_data['lab'][lab_name].values())])
+    return data
+
+
+def prepare_liner_data(meta_data):
+    # format example [new Date(2015, 0, 1), 5]
+    duplicate = {}
+    data = []
+    for lab_name in meta_data['lab']:
+        for day in meta_data['lab'][lab_name]:
+            if day in duplicate:
+                duplicate[day] += meta_data['lab'][lab_name][day]
+            else:
+                duplicate[day] = meta_data['lab'][lab_name][day]
+
+    for day in duplicate:
+        data.append([int(time.mktime(datetime.datetime(int(day.split("/")[0]), int(day.split("/")[1]),
+                                                       int(day.split("/")[2])).timetuple())) * 1000,
+                     duplicate[day]])
+    return data

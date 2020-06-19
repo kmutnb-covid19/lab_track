1b1fa1ff849e68f8c1f440093e43f3dfb5f10b80
Nattakrit Jatupattaradit <s6101012610037@email.kmutnb.ac.th>
2020-06-20 04:02:03 +0700

    Hide sensitive data in settings.py by moving to .env file

--- kmutnbtrack/settings.py
+++ kmutnbtrack/settings.py
@@ -11,22 +11,25 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# load environment variables from .env
+dot_env_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dot_env_file):
+    dotenv.load_dotenv(dot_env_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '***REMOVED***'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["labtrack.cony.codes", "localhost", "127.0.0.1"]
-
 
 # Application definition
 
@@ -71,8 +74,6 @@ TEMPLATES = [
     },
 ]
 
-
-
 WSGI_APPLICATION = 'kmutnbtrack.wsgi.application'
 SESSION_ENGINE = 'django.contrib.sessions.backends.file'
 
@@ -99,7 +100,6 @@ CACHES = {
 
 QR_CODE_CACHE_ALIAS = 'qr-code'
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -123,8 +123,8 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '***REMOVED***'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '***REMOVED***'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ['SOCIAL_AUTH_GOOGLE_OAUTH2_KEY']
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ['SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET']
 
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
@@ -145,7 +145,6 @@ LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL = '/'
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -160,28 +159,27 @@ DATETIME_FORMAT = 'N j, Y, H:i'
 
 USE_TZ = True
 
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 
-
 DEFAULT_FROM_EMAIL = 'KMUTNB labtrack <noreply@labtrack.cony.codes>'
+
 '''EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = '***REMOVED***'
+EMAIL_HOST_PASSWORD = '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'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-'''
+EMAIL_USE_TLS = True'''
+# Sendgrid api
 EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-SENDGRID_API_KEY = '***REMOVED***'
+SENDGRID_API_KEY = os.environ['SENDGRID_API_KEY']
 SENDGRID_SANDBOX_MODE_IN_DEBUG = False
+
 # Back up Database
 DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
 DBBACKUP_STORAGE_OPTIONS = {'location': os.path.join(BASE_DIR, 'backups')}
